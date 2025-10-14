79ff53fb890f29eeb1258609f0481ac0ca7941c6
GuilleDev <guillermo.unzueta@gmail.com>
2025-10-14 18:53:24 +0200

    VP

--- Report_ACL.ps1
+++ Report_ACL.ps1
@@ -1,10 +1,17 @@
 Import-Module ActiveDirectory
 
-Get-ADUser -Filter * -Properties adminCount, ntSecurityDescriptor |
-Where-Object {
-    $_.adminCount -eq 1 -or
-    $_.ntSecurityDescriptor.AreAccessRulesProtected -eq $true
-} |
-Select-Object Name, DistinguishedName, adminCount,
-    @{Name='IsProtected';Expression={$_.ntSecurityDescriptor.AreAccessRulesProtected}} |
-Export-Csv "ProtectedOrAdminCount.csv" -NoTypeInformation -Encoding UTF8BOM 
+$Forest = [System.DirectoryServices.ActiveDirectory.Forest]::GetCurrentForest()
+
+$Report = @()
+Foreach ($Domain in $Forest.Domains) {
+    $Report += Get-ADUser -Filter * -Properties adminCount, ntSecurityDescriptor -server $Domain.PDCRoleOwner.name `
+    | Where-Object {
+        $_.adminCount -eq 1 -or
+        $_.ntSecurityDescriptor.AreAccessRulesProtected -eq $true
+    } `
+    | Select-Object Name, DistinguishedName, adminCount, @{Name = 'IsProtected'; Expression = { $_.ntSecurityDescriptor.AreAccessRulesProtected } }
+}
+
+ 
+$Report | Export-Csv "ProtectedOrAdminCount.csv" -NoTypeInformation -Encoding UTF8BOM 
+
