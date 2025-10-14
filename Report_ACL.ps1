e1a60504fcdfa7796d0de8f2f64cdb4ab1a7e8c3
GuilleDev <guillermo.unzueta@gmail.com>
2025-10-14 18:25:07 +0200

    UTF8BOM

--- Report_ACL.ps1
+++ Report_ACL.ps1
@@ -7,4 +7,4 @@ Where-Object {
 } |
 Select-Object Name, DistinguishedName, adminCount,
     @{Name='IsProtected';Expression={$_.ntSecurityDescriptor.AreAccessRulesProtected}} |
-Export-Csv "ProtectedOrAdminCount.csv" -NoTypeInformation
+Export-Csv "ProtectedOrAdminCount.csv" -NoTypeInformation -Encoding UTF8BOM 
